354ff63ccd461573eb580fcc6f6d6bf7d1406c83
zongwu <zongwu77@126.com>
2017-10-15 23:34:41 +0800

    提交博客的详情，侧栏的功能

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
+import markdown
 from django.shortcuts import render, get_object_or_404
-from .models import Post 
+from .models import Post, Category 
 # Create your views here.
 def index(request):
 	post_list = Post.objects.all().order_by('-create_time')
 	return render(request, 'blog/index.html', context={'post_list': post_list})
 
+
 def detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
+	post.body = markdown.markdown(post.body, extensions=[
+			'markdown.extensions.extra',
+			'markdown.extensions.codehilite',
+			'markdown.extensions.toc',
+		])
 	return render(request, 'blog/detail.html', context={'post': post})
+
+
+def archives(request, year, month):
+	post_list = Post.objects.filter(
+		create_time__year = year, 
+		create_time__month = month).order_by('-create_time')
+	return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+	cate = get_object_or_404(Category, pk=pk)
+	post_list = Post.objects.filter(category=cate).order_by('-create_time')
+	return render(request, 'blog/index.html', context={'post_list': post_list})
