93a937969f32d58d8eba3476aeba586468c007b7
zongwu <zongwu77@126.com>
2017-10-18 23:37:37 +0800

    提交完整的评论功能

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category 
+
+#评论相关
+from comments.forms import CommentForm
+
 # Create your views here.
 def index(request):
 	post_list = Post.objects.all().order_by('-create_time')
@@ -14,7 +18,10 @@ def detail(request, pk):
 			'markdown.extensions.codehilite',
 			'markdown.extensions.toc',
 		])
-	return render(request, 'blog/detail.html', context={'post': post})
+	form = CommentForm()
+	comment_list = post.comment_set.all()
+	context = {'post': post, 'form': form, 'comment_list': comment_list}
+	return render(request, 'blog/detail.html', context=context)
 
 
 def archives(request, year, month):
