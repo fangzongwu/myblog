18412af4e089c1539574760f44fd19fdc29432ee
zongwu <zongwu77@126.com>
2017-10-20 22:45:29 +0800

    提交包括评论的完整代码

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from comments.forms import CommentForm
 
 # Create your views here.
 def index(request):
-	post_list = Post.objects.all().order_by('-create_time')
+	post_list = Post.objects.all()
 	return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
@@ -27,10 +27,10 @@ def detail(request, pk):
 def archives(request, year, month):
 	post_list = Post.objects.filter(
 		create_time__year = year, 
-		create_time__month = month).order_by('-create_time')
+		create_time__month = month)
 	return render(request, 'blog/index.html', context={'post_list': post_list})
 
 def category(request, pk):
 	cate = get_object_or_404(Category, pk=pk)
-	post_list = Post.objects.filter(category=cate).order_by('-create_time')
+	post_list = Post.objects.filter(category=cate)
 	return render(request, 'blog/index.html', context={'post_list': post_list})
