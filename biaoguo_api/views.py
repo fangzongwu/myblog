1ea962a55e15bcf1540e451459dda02dc895b5e3
zongwu <zongwu77@126.com>
2018-07-25 12:28:43 +0800

    订单的降序排列

--- biaoguo_api/views.py
+++ biaoguo_api/views.py
@@ -12,7 +12,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
 # 订单价格
 class TotalPriceViewSet(viewsets.ModelViewSet):
 	# ModelViewSet模型默认拥有get、post请求
-	queryset = TotalPrice.objects.all()
+	queryset = TotalPrice.objects.all().order_by('-create_date')
 	serializer_class = TotalPriceSerializer
 
 # 推荐美食
