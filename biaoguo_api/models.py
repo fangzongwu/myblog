a7db6070f060f1612322c3c128225f3f386aff42
zongwu <zongwu77@126.com>
2018-07-25 16:03:14 +0800

    提交order排序问题

--- biaoguo_api/models.py
+++ biaoguo_api/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 # 订单
 class TotalPrice(models.Model):
 	total = models.FloatField()
-	create_date = models.DateField(auto_now_add=True)
+	create_date = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.total
